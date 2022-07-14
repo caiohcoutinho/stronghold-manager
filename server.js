fb67e7927808db25be714eeabacbf9f3470d6e19
Caio Coutinho <caiohcoutinho@gmail.com>
2022-07-14 07:04:52 -0300

    vue.js
    Change-Id: I7eaa4329c8c66614a6562d6e102f123d687f2520

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var app = express();
+const path = require('path');
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'static/components/landing/landing.html'));
+});
+
+app.use(express.static(path.join(__dirname, 'static')));
 
 var server = app.listen(8081, function () {
    var host = server.address().address
